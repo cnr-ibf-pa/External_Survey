7c1cae3ae923977b38eaa86bf779dccc04383b01
Stefania Pantina <stefania.pantina@gmail.com>
2019-11-07 10:29:26 +0100

    update auth

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
-function init() {
 let client = new jso.JSO({
-	providerID: "HBP",
-	client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
-  redirect_uri: "https://stefysp.github.io/External_Survey/",
+  providerID: "HBP",
+  client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
+  redirect_uri: "https://stefysp.github.io/External_Survey/", // The URL where you is redirected back, and where you perform run the callback() function.
   authorization: "https://services.humanbrainproject.eu/oidc/authorize",
 })
 
+
+function init() {
   try {
     client.callback();
   } catch (e) {
@@ -14,13 +15,15 @@ let client = new jso.JSO({
 
 const USER_API = 'https://services.humanbrainproject.eu/idm/v1/api/user/me';
 var authorization = client.getToken();
-
+  
+console.log(authorization)
+  
 authorization.then((session) => {
 var header = {'headers' : {Authorization: 'Bearer ' + session.access_token}};
 document.getElementById("hbp-token").innerHTML = session.access_token;
-
+  
 console.log(header);
-	
+  
 $.ajax({
     url: USER_API,
     headers: {
@@ -29,13 +32,15 @@ $.ajax({
     },
     method: 'GET',
     success: function(data){
-	    console.log(data.id);
-       document.getElementById("hbp-user-data").innerHTML = JSON.stringify(data);
-	  document.getElementById("hbp-user-id").innerHTML = data.id;
+      console.log(data.id);
+        document.getElementById("hbp-user-data").innerHTML = JSON.stringify(data);
+  document.getElementById("hbp-user-id").innerHTML = data.id;
     }
 });
 }); 
-	 return authorization;
+   return authorization;
 }
+       
 
+init();
 
