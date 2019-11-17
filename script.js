7e2b0775c3d170a5124bd6c7b4642a1080d0c8c0
Stefysp <stefaniapantina@gmail.com>
2019-11-17 11:06:35 +0100

    update

--- script.js
+++ script.js
@@ -86,6 +86,7 @@ function formSubmitted(status) {
 
 function find_answer(checkboxes){
    		var vals = "";
+   		
 		for (var i=0, n=checkboxes.length;i<n;i++) {
     		if (checkboxes[i].checked){
         		//vals.push(checkboxes[i].value);
