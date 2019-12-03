516920fe6368623748527893929e7842df6adb7a
Stefania Pantina <stefania.pantina@gmail.com>
2019-12-03 12:25:13 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-
   let client = new jso.JSO({
   providerID: "HBP",
   client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
   redirect_uri: "https://cnr-ibf-pa.github.io/External_Survey/", // The URL where you is redirected back, and where you perform run the callback() function.
   authorization: "https://services.humanbrainproject.eu/oidc/authorize",
+  scopes: { request: ["https://services.humanbrainproject.eu/oidc/profile"]}
 })
 
 function init() {
@@ -40,7 +40,8 @@ $.ajax({
     }
 });
 }); 
-     return authorization;
+    client.wipeTokens();
+    return authorization;
 }
 
 
