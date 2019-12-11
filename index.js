fc6899ec4fe03fc8110a1b75b3aac42f151fc3f7
Stefysp <stefaniapantina@gmail.com>
2019-12-11 12:37:56 +0100

    update

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ function postContactToGoogle() {
     //UserID
     var userId=$('#hbp-user-id').val();
 
-    var IpAdress=$('#ip_address').val();
+    var IpAddress=$('#ip_address').val();
 
     var browser=$('#browser').val();
 
@@ -238,7 +238,7 @@ function postContactToGoogle() {
       "entry_363719983":MV3,"entry_840494516":MV4,"entry_1006444071":MV5,"entry_1691163079":NSG,"entry_1556173154":jureca,
       "entry_1527194443":pizD,"entry_152105565":marconi,"entry_825970456":F_ML,"entry_1973597846":F_SM,"entry_389249447":F_TA,
       "entry_275038741":F_MA,"entry_569020820":F_SCB,"entry_2060872419":F_CB,"entry_1738577912":F_SCISE,"entry_1692693658":F_SmCISE,
-      "entry_1545394113":F_BACISE,"entry_1064567374":F_MV,"entry_650601249":F_HIW,"entry_650664193":HIW1,"entry_1573033199":IpAdress,
+      "entry_1545394113":F_BACISE,"entry_1064567374":F_MV,"entry_650601249":F_HIW,"entry_650664193":HIW1,"entry_1573033199":IpAddress,
       "entry_224041548":browser},
       type:"POST",dataType:"xml",statusCode: {0:function() { 
         window.location.replace("thankyou.html");},200:function(){window.location.replace("thankyou.html");}}
@@ -250,54 +250,10 @@ function WhereIs(){
     init();
     var sBrowser=browser_detect();
     document.getElementById("browser").innerHTML=sBrowser;
-    getUserIP(function(ip){
-    document.getElementById("ip_address").innerHTML=ip;
-    console.log(ip);
-    });
+    getIPAddress();
     }
 }
-/**
- * Get the user IP throught the webkitRTCPeerConnection
- * @param onNewIP {Function} listener function to expose the IP locally
- * @return undefined
- */
-function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-    //compatibility for firefox and chrome
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    var pc = new myPeerConnection({
-        iceServers: []
-    }),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
 
-    function iterateIP(ip) {
-        if (!localIPs[ip]) onNewIP(ip);
-        localIPs[ip] = true;
-    }
-
-     //create a bogus data channel
-    pc.createDataChannel("");
-
-    // create offer and set local description
-    pc.createOffer().then(function(sdp) {
-        sdp.sdp.split('\n').forEach(function(line) {
-            if (line.indexOf('candidate') < 0) return;
-            line.match(ipRegex).forEach(iterateIP);
-        });
-        
-        pc.setLocalDescription(sdp, noop, noop);
-    }).catch(function(reason) {
-        // An error occurred, so handle the failure to connect
-    });
-
-    //listen for candidate events
-    pc.onicecandidate = function(ice) {
-        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-    };
-}
 
 function browser_detect(){
     var sBrowser, sUsrAg = navigator.userAgent;
@@ -330,6 +286,12 @@ function browser_detect(){
     }
     return sBrowser;
 }
+
+ var getIPAddress = function() {
+      $.getJSON("https://jsonip.com?callback=?", function(data) {
+        document.getElementById("ip_address").innerHTML=data.ip;
+      });
+    };
 
 $(document).ready(function () {
     WhereIs();
