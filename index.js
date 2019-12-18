f6056850eba15a9b922e5b344a3adc9f7f66ac24
Stefania Pantina <stefania.pantina@gmail.com>
2019-12-18 14:42:37 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let client = new jso.JSO({
   providerID: "HBP",
   client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
-  redirect_uri: "https://cnr-ibf-pa.github.io/External_Survey/", // The URL where you is redirected back, and where you perform run the callback() function.
+  redirect_uri: "https://cnr-ibf-pa.github.io/hbp-bsp-user-survey-dev/", // The URL where you is redirected back, and where you perform run the callback() function.
   authorization: "https://services.humanbrainproject.eu/oidc/authorize",
 })  
 
