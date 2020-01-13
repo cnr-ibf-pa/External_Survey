a8f38de310569eee8d61477dc16b73a0f353e898
Stefysp <stefaniapantina@gmail.com>
2020-01-13 15:04:14 +0100

    update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
-function init() {
-    let client = new jso.JSO({
-        providerID: "HBP",
-        client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
-        redirect_uri: "https://cnr-ibf-pa.github.io/hbp-bsp-user-survey-dev/", // The URL where you is redirected back, and where you perform run the callback() function.
-        authorization: "https://services.humanbrainproject.eu/oidc/authorize",}) 
-    }
+var pathArray = window.location.pathname.split('/');
+console.log(pathArray[1]);
 
+let client = new jso.JSO({
+  providerID: "HBP",
+  client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
+  redirect_uri: "https://cnr-ibf-pa.github.io/hbp-bsp-user-survey-dev/", // The URL where you is redirected back, and where you perform run the callback() function.
+  authorization: "https://services.humanbrainproject.eu/oidc/authorize",}) 
+
+function init() {
     try {
     client.callback();
   } catch (e) {
@@ -262,8 +264,6 @@ function postContactToGoogle() {
 
 function WhereIs(){
   if(window != top){
-    var pathArray = window.location.pathname.split('/');
-    console.log(pathArray[1]);
     init();
     }
 }
