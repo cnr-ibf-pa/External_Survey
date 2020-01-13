7174cb6c626e0f0beb1566669a4862a103cd5c12
Stefysp <stefaniapantina@gmail.com>
2020-01-13 16:58:35 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
  
-function init() {
+function init_dev() {
+
+    let client = new jso.JSO({
+        providerID: "HBP",
+        client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
+        redirect_uri: "https://cnr-ibf-pa.github.io/hbp-bsp-user-survey-dev/", // The URL where you is redirected back, and where you perform run the callback() function.
+        authorization: "https://services.humanbrainproject.eu/oidc/authorize",})
+
     try {
     client.callback();
   } catch (e) {
@@ -258,14 +265,8 @@ function WhereIs(){
   if(window != top){
     var pathArray = window.location.pathname.split('/');
     if (pathArray[1]=='hbp-bsp-user-survey-dev'){
-        let client = new jso.JSO({
-        providerID: "HBP",
-        client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
-        redirect_uri: "https://cnr-ibf-pa.github.io/hbp-bsp-user-survey-dev/", // The URL where you is redirected back, and where you perform run the callback() function.
-        authorization: "https://services.humanbrainproject.eu/oidc/authorize",})
+      init_dev();  
     }
-
-    init();
     }
 }
 
